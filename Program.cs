cf72ab47e983126b857658c1cd85fb3721277869
Sam <snippetsbysam@outlook.com>
2022-06-06 02:04:37 +0100

    skeleton

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MarsRoverChallenge;
+
 Console.WriteLine("Hello, World!");
+//Request line inputs
+
+Vehicle rover = new Vehicle(1, 2, Orientation.N);
+
+rover.Move("LMLMLMLMM");
---
+++ Vehicle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverChallenge
+{
+    public enum Orientation
+    {
+        N = 0,
+        E = 1,
+        S = 2,
+        W = 3
+    }
+    public class Vehicle
+    {
+        private int XPosition;
+        private int YPosition;
+        private Orientation Heading;
+
+        public Vehicle(int xPosition, int yPosition, Orientation heading)
+        {
+            XPosition = xPosition;
+            YPosition = yPosition;
+            Heading = heading;
+        }
+
+        public void Move(string instructions)
+        {
+            Console.WriteLine($"{XPosition} {YPosition} {Heading.ToString()}");
+        }
+
+        public void MoveForward()
+        {
+            switch (Heading)
+            {
+                case Orientation.N:
+                    YPosition++;
+                    break;
+                case Orientation.E:
+                    XPosition++;
+                    break;
+                case Orientation.S:
+                    YPosition--;
+                    break;
+                case Orientation.W:
+                    XPosition--;
+                    break;
+            }
+        }
+
+        public void RotateLeft()
+        {
+
+        }
+
+        public void RotateRight()
+        {
+            Console.WriteLine($"Heading Before {Heading.ToString()}");
+        }
+    }
+}
